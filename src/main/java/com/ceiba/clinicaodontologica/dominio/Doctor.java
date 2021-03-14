9ab86e7749c7ba171f8fd2e55c2d17e481aa7881
Juan Guillermo Díaz Martinez - Ceiba Software <juan.diaz@ceiba.com.co>
2021-03-14 11:41:43 -0500

    Adicion de correcciones 18

--- src/main/java/com/ceiba/clinicaodontologica/dominio/Doctor.java
+++ src/main/java/com/ceiba/clinicaodontologica/dominio/Doctor.java
@@ -2,55 +2,28 @@ package com.ceiba.clinicaodontologica.dominio;
 
 import com.ceiba.clinicaodontologica.dominio.validador.parametro.ValidarParametro;
 
-public class Doctor {
+public class Doctor extends Persona{
 
 	private static final String INGRESAR_CODIGO_DOCTOR = "Se debe ingresar el codigo del doctor";
 	private static final String INGRESAR_NOMBRE_DOCTOR = "Se debe ingresar el nombre del doctor";
 	private static final String INGRESAR_APELLIDO_DOCTOR = "Se debe ingresar el apellido del doctor";
 	private static final String INGRESAR_ESPECIALIZACION_DOCTOR = "Se debe ingresar la especializacion del doctor";
 	
-    private Integer codigo;
-    private String nombre;
-    private String apellido;
     private String especializacion;
     
 	public Doctor() {}
     
     public Doctor(Integer codigo, String nombre, String apellido, String especializacion) {
     	
-    	ValidarParametro.validarObjeto(codigo, INGRESAR_CODIGO_DOCTOR);
+    	super(codigo, nombre, apellido);
+    	
+		ValidarParametro.validarObjeto(codigo, INGRESAR_CODIGO_DOCTOR);
     	ValidarParametro.validarObligatorio(nombre, INGRESAR_NOMBRE_DOCTOR);
     	ValidarParametro.validarObligatorio(apellido, INGRESAR_APELLIDO_DOCTOR);
     	ValidarParametro.validarObligatorio(especializacion, INGRESAR_ESPECIALIZACION_DOCTOR);
     	
-		this.codigo = codigo;
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.especializacion = especializacion;
-	}
-
-	public Integer getCodigo() {
-		return codigo;
-	}
-
-	public void setCodigo(Integer codigo) {
-		this.codigo = codigo;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getApellido() {
-		return apellido;
-	}
-
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
+    	this.especializacion = especializacion;
+		
 	}
 
 	public String getEspecializacion() {
--- src/main/java/com/ceiba/clinicaodontologica/dominio/Paciente.java
+++ src/main/java/com/ceiba/clinicaodontologica/dominio/Paciente.java
@@ -2,7 +2,7 @@ package com.ceiba.clinicaodontologica.dominio;
 
 import com.ceiba.clinicaodontologica.dominio.validador.parametro.ValidarParametro;
 
-public class Paciente {
+public class Paciente extends Persona{
 
 	private static final String INGRESAR_CODIGO_PACIENTE = "Se debe ingresar el codigo del paciente";
 	private static final String INGRESAR_NOMBRE_PACIENTE = "Se debe ingresar el nombre del paciente";
@@ -10,9 +10,6 @@ public class Paciente {
 	private static final String INGRESAR_EDAD_PACIENTE = "Se debe ingresar la edad del paciente";
 	private static final String INGRESAR_TELEFONO_PACIENTE = "Se debe ingresar el telefono del paciente";
 	
-    private Integer codigo;
-    private String nombre;
-    private String apellido;
     private Integer edad;
     private String telefono;
 	
@@ -20,41 +17,16 @@ public class Paciente {
     
     public Paciente(Integer codigo, String nombre, String apellido, Integer edad, String telefono) {
     	
+    	super(codigo,nombre,apellido);
+    	
     	ValidarParametro.validarObjeto(codigo, INGRESAR_CODIGO_PACIENTE);
     	ValidarParametro.validarObligatorio(nombre, INGRESAR_NOMBRE_PACIENTE);
     	ValidarParametro.validarObligatorio(apellido, INGRESAR_APELLIDO_PACIENTE);
     	ValidarParametro.validarObjeto(edad, INGRESAR_EDAD_PACIENTE);
     	ValidarParametro.validarObligatorio(telefono, INGRESAR_TELEFONO_PACIENTE);
     	
-		this.codigo = codigo;
-		this.nombre = nombre;
-		this.apellido = apellido;
 		this.edad = edad;
 		this.telefono = telefono;
-	}
-
-	public Integer getCodigo() {
-		return codigo;
-	}
-
-	public void setCodigo(Integer codigo) {
-		this.codigo = codigo;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getApellido() {
-		return apellido;
-	}
-
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
 	}
 
 	public Integer getEdad() {
@@ -72,5 +44,5 @@ public class Paciente {
 	public void setTelefono(String telefono) {
 		this.telefono = telefono;
 	}
-    
+
 }
