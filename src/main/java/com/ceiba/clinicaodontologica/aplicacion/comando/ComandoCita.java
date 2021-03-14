eabc538774daa9a97406214e91b135500ef608dc
Juan Guillermo Díaz Martinez - Ceiba Software <juan.diaz@ceiba.com.co>
2021-03-14 10:36:51 -0500

    Adicion de correcciones 15

--- src/main/java/com/ceiba/clinicaodontologica/aplicacion/comando/ComandoCita.java
+++ src/main/java/com/ceiba/clinicaodontologica/aplicacion/comando/ComandoCita.java
@@ -2,11 +2,12 @@ package com.ceiba.clinicaodontologica.aplicacion.comando;
 
 import java.util.Date;
 
+import com.ceiba.clinicaodontologica.dominio.Cita;
 import com.ceiba.clinicaodontologica.dominio.Doctor;
 import com.ceiba.clinicaodontologica.dominio.Factura;
 import com.ceiba.clinicaodontologica.dominio.Paciente;
 
-public class ComandoCita {
+public class ComandoCita extends Cita{
 
 	private String procedimiento;
 
@@ -18,54 +19,50 @@ public class ComandoCita {
 
 	private Factura factura;
 
-	public ComandoCita() {}
+//	public ComandoCita() {}
 	
 	public ComandoCita(String procedimiento, Date fechaCita, Paciente paciente, Doctor doctor,
 			Factura factura) {
-		this.procedimiento = procedimiento;
-		this.fechaCita = fechaCita;
-		this.paciente = paciente;
-		this.doctor = doctor;
-		this.factura = factura;
+		super(procedimiento,fechaCita,paciente,doctor,factura);
 	}
 
-	public String getProcedimiento() {
-		return procedimiento;
-	}
-
-	public void setProcedimiento(String procedimiento) {
-		this.procedimiento = procedimiento;
-	}
-
-	public Date getFechaCita() {
-		return (Date) fechaCita.clone();
-	}
-
-	public void setFechaCita(Date fechaCita) {
-		this.fechaCita = (Date) fechaCita.clone();
-	}
-
-	public Paciente getPaciente() {
-		return paciente;
-	}
-
-	public void setPaciente(Paciente paciente) {
-		this.paciente = paciente;
-	}
-
-	public Doctor getDoctor() {
-		return doctor;
-	}
-
-	public void setDoctor(Doctor doctor) {
-		this.doctor = doctor;
-	}
-
-	public Factura getFactura() {
-		return factura;
-	}
-
-	public void setFactura(Factura factura) {
-		this.factura = factura;
-	}
+//	public String getProcedimiento() {
+//		return procedimiento;
+//	}
+//
+//	public void setProcedimiento(String procedimiento) {
+//		this.procedimiento = procedimiento;
+//	}
+//
+//	public Date getFechaCita() {
+//		return (Date) fechaCita.clone();
+//	}
+//
+//	public void setFechaCita(Date fechaCita) {
+//		this.fechaCita = (Date) fechaCita.clone();
+//	}
+//
+//	public Paciente getPaciente() {
+//		return paciente;
+//	}
+//
+//	public void setPaciente(Paciente paciente) {
+//		this.paciente = paciente;
+//	}
+//
+//	public Doctor getDoctor() {
+//		return doctor;
+//	}
+//
+//	public void setDoctor(Doctor doctor) {
+//		this.doctor = doctor;
+//	}
+//
+//	public Factura getFactura() {
+//		return factura;
+//	}
+//
+//	public void setFactura(Factura factura) {
+//		this.factura = factura;
+//	}
 }
